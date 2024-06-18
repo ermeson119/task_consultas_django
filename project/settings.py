11aa9e22d46ac4dabfc9d7559609f1200c3cd531
ermeson119 <ermesonbalbinot119@gmail.com>
2024-06-18 18:30:53 -0300

    teste

--- project/settings.py
+++ project/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
-    },
+    },  
 ]
 
 WSGI_APPLICATION = 'project.wsgi.application'
